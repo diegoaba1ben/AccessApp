340c34f3331a1b7c24acc272371dd97997fd038e
Diego Benjumea <diegobenjumeafreelance@outlook.com>
2025-01-25 13:08:34 -0500

    1.0.00.Creación de proyecto, servicio de usuario y pruebas, instalación de dependencias para pruebas , creación de estructura de directorios

---
+++ Test/AccessAppUser.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace AccessAppUser.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true); // Asserción b´sica para la configuración de pruebas
+    }
+}
